f574406f992b67a1faf4c27651e5529293679e5e
RyUmarof <ryumarof@gmail.com>
2024-10-30 18:29:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let a = prompt('Ismingizni kiriting')
+let b = prompt('id kiriting')
+let c = prompt('parolingizni kiriting')
+
+let data = {
+         
+         name: 'abdulloh',
+         id: 123,
+         parol: 124,
+         
+         name: a,
+         id: b,
+         parol: c,
+
+         
+}
+
+
+console.log(data);
+
+
+
